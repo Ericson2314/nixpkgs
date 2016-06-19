ea7b16ec4372cd6d5c6b94e3165b10acfa8aae73
John Ericson <Ericson2314@Yahoo.com>
2016-06-19 00:26:15 -0700

    Fix indentation

--- pkgs/top-level/default.nix
+++ pkgs/top-level/default.nix
@@ -85,79 +85,79 @@ in let
       inherit system allPackages platform config crossSystem lib;
     };
 
-/* This composes a single bootstrapping phase of the Nix Packages
-   collection. That is, it imports the functions that build the various
-   packages, and calls them with appropriate arguments. The result is a set of
-   all the packages in the Nix Packages collection for some particular platform
-   for some particular phase.
- */
+  /* This composes a single bootstrapping phase of the Nix Packages
+     collection. That is, it imports the functions that build the various
+     packages, and calls them with appropriate arguments. The result is a set of
+     all the packages in the Nix Packages collection for some particular platform
+     for some particular phase.
+   */
 
 
-top-level = { system, stdenv, allowCustomOverrides ? true, config, crossSystem, platform}:
-let
-  # Allow packages to be overridden globally via the `packageOverrides'
-  # configuration option, which must be a function that takes `pkgs'
-  # as an argument and returns a set of new or overridden packages.
-  # The `packageOverrides' function is called with the *original*
-  # (un-overridden) set of packages, allowing packageOverrides
-  # attributes to refer to the original attributes (e.g. "foo =
-  # ... pkgs.foo ...").
-  configOverrider = self: config.packageOverrides or (super: {});
-
-  # Return the complete set of packages, after applying the overrides
-  # returned by the `overrider' function (see above).  Warning: this
-  # function is very expensive!
-  pkgsWithOverrides = overrider:
+  top-level = { system, stdenv, allowCustomOverrides ? true, config, crossSystem, platform}:
     let
-      stdenvAdapters = self: super:
-        let res = import ../stdenv/adapters.nix self; in res // {
-          stdenvAdapters = res;
-        };
+      # Allow packages to be overridden globally via the `packageOverrides'
+      # configuration option, which must be a function that takes `pkgs'
+      # as an argument and returns a set of new or overridden packages.
+      # The `packageOverrides' function is called with the *original*
+      # (un-overridden) set of packages, allowing packageOverrides
+      # attributes to refer to the original attributes (e.g. "foo =
+      # ... pkgs.foo ...").
+      configOverrider = self: config.packageOverrides or (super: {});
 
-      trivialBuilders = self: super:
-        (import ../build-support/trivial-builders.nix {
-          inherit lib; inherit (self) stdenv; inherit (self.xorg) lndir;
-        });
+      # Return the complete set of packages, after applying the overrides
+      # returned by the `overrider' function (see above).  Warning: this
+      # function is very expensive!
+      pkgsWithOverrides = overrider:
+        let
+          stdenvAdapters = self: super:
+            let res = import ../stdenv/adapters.nix self; in res // {
+              stdenvAdapters = res;
+            };
 
-      stdenvDefault = let
-          stdenv_ = stdenv;
-          changer = config.replaceStdenv or null;
-        in { stdenv = stdenv_ // { inherit platform; }; };
+          trivialBuilders = self: super:
+            (import ../build-support/trivial-builders.nix {
+              inherit lib; inherit (self) stdenv; inherit (self.xorg) lndir;
+            });
 
-      allPackagesArgs = {
-        inherit system config crossSystem platform lib
-          pkgsWithOverrides mkPackages;
-      };
-      allPackages = self: super:
-        let res = import ./all-packages.nix allPackagesArgs res self;
-        in res;
+          stdenvDefault = let
+              stdenv_ = stdenv;
+              changer = config.replaceStdenv or null;
+            in { stdenv = stdenv_ // { inherit platform; }; };
 
-      aliases = self: super: import ./aliases.nix super;
+          allPackagesArgs = {
+            inherit system config crossSystem platform lib
+              pkgsWithOverrides mkPackages;
+          };
+          allPackages = self: super:
+            let res = import ./all-packages.nix allPackagesArgs res self;
+            in res;
 
-      # stdenvOverrides is used to avoid circular dependencies for building
-      # the standard build environment. This mechanism uses the override
-      # mechanism to implement some staged compilation of the stdenv.
-      #
-      # We don't want stdenv overrides in the case of cross-building, or
-      # otherwise the basic overridden packages will not be built with the
-      # crossStdenv adapter.
-      stdenvOverrides = self: super:
-        lib.optionalAttrs (crossSystem == null && super.stdenv ? overrides)
-          (super.stdenv.overrides super);
+          aliases = self: super: import ./aliases.nix super;
 
-      customOverrides = self: super:
-        lib.optionalAttrs allowCustomOverrides (overrider self super);
+          # stdenvOverrides is used to avoid circular dependencies for building
+          # the standard build environment. This mechanism uses the override
+          # mechanism to implement some staged compilation of the stdenv.
+          #
+          # We don't want stdenv overrides in the case of cross-building, or
+          # otherwise the basic overridden packages will not be built with the
+          # crossStdenv adapter.
+          stdenvOverrides = self: super:
+            lib.optionalAttrs (crossSystem == null && super.stdenv ? overrides)
+              (super.stdenv.overrides super);
+
+          customOverrides = self: super:
+            lib.optionalAttrs allowCustomOverrides (overrider self super);
+        in
+          lib.fix' (
+            lib.extends customOverrides (
+              lib.extends stdenvOverrides (
+                lib.extends aliases (
+                  lib.extends allPackages (
+                    lib.extends trivialBuilders (
+                      lib.extends stdenvAdapters (
+                        self: stdenvDefault)))))));
     in
-      lib.fix' (
-        lib.extends customOverrides (
-          lib.extends stdenvOverrides (
-            lib.extends aliases (
-              lib.extends allPackages (
-                lib.extends trivialBuilders (
-                  lib.extends stdenvAdapters (
-                    self: stdenvDefault)))))));
-in
-  pkgsWithOverrides configOverrider;
+      pkgsWithOverrides configOverrider;
 
   pkgs = allPackages { inherit system stdenv config crossSystem platform; };
 
