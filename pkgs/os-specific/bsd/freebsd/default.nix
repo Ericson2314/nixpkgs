f218c590b6502b72dd49f94f0845dbcef6dffe8d
John Ericson <John.Ericson@Obsidian.Systems>
2022-10-06 01:36:35 -0400

    freebsd.rpcgen: Remove terrible hack

--- pkgs/os-specific/bsd/freebsd/default.nix
+++ pkgs/os-specific/bsd/freebsd/default.nix
@@ -497,18 +497,23 @@ in lib.makeScopeWithSplicing
 
   rpcgen = mkDerivation rec {
     path = "usr.bin/rpcgen";
-    # for debugging
-    # makeFlags = [ "-d x" ];
-    # NIX_DEBUG = 7;
-
-    # This is terrible! No idea why this macro definition got lost.
-    #
-    #     echo -e '#include <sys/wait.h>\nWUNTRACED' | $CC -E -x c - | tail
-    #
-    # Actually works just fine in this derivation.
-    postPatch = ''
-      substituteInPlace $BSD_PATH/rpc_scan.c --replace WUNTRACED 2
-    '';
+    patches = lib.optionals (stdenv.hostPlatform.libc == "glibc") [
+      # `WUNTRACED` is defined privately `bits/waitflags.h` in glibc.
+      # But instead of having a regular header guard, it has some silly
+      # non-modular logic. `stdlib.h` will include it if `sys/wait.h`
+      # hasn't yet been included (for it would first), and vice versa.
+      #
+      # The problem is that with the FreeBSD compat headers, one of
+      # those headers ends up included other headers...which ends up
+      # including the other one, this means by the first time we reach
+      # `#include `<bits/waitflags.h>`, both `_SYS_WAIT_H` and
+      # `_STDLIB_H` are already defined! Thus, we never ned up including
+      # `<bits/waitflags.h>` and defining `WUNTRACED`.
+      #
+      # This hacks around this by manually including `WUNTRACED` until
+      # the problem is fixed properly in glibc.
+      ./rpcgen-glibc-hack.patch
+    ];
   };
 
   gencat = mkDerivation {
