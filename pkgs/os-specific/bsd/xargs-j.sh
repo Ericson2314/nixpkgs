dee715bf88bce3116cad29f57573b25038e5c082
John Ericson <John.Ericson@Obsidian.Systems>
2022-10-27 10:51:40 -0400

    freebsd.xargs-j: tabs -> spaces

--- pkgs/os-specific/bsd/xargs-j.sh
+++ pkgs/os-specific/bsd/xargs-j.sh
@@ -7,7 +7,7 @@ shift
 
 while (( $# )); do
     if [[ "$1" = "$token" ]]; then
-		mapfile -t -O $(("${#args[@]}" + 1)) args
+        mapfile -t -O $(("${#args[@]}" + 1)) args
     else
         args+=("$1")
     fi
