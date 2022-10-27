6f52db70cadc80a1f5979579b10b1ab549e3aee2
9glenda <plan9git@proton.me>
2022-10-27 19:53:30 +0000

    Minior changes to shell code

--- pkgs/os-specific/bsd/freebsd/default.nix
+++ pkgs/os-specific/bsd/freebsd/default.nix
@@ -49,7 +49,7 @@ let
 
     while (( $# )); do
       if (( $# == 1 )); then
-        if (( $path_args > 1)) || [[ "$1" = */ ]]; then
+        if (( path_args > 1)) || [[ "$1" = */ ]]; then
           mkdir -p "$1"
         else
           mkdir -p "$(dirname "$1")"
@@ -188,10 +188,10 @@ in lib.makeScopeWithSplicing
     installPhase = ''
       runHook preInstall
 
-      install -D bmake $out/bin/bmake
-      ln -s $out/bin/bmake $out/bin/make
-      mkdir -p $out/share
-      cp -r $BSDSRCDIR/share/mk $out/share/mk
+      install -D bmake "$out/bin/bmake"
+      ln -s "$out/bin/bmake" "$out/bin/make"
+      mkdir -p "$out/share"
+      cp -r "$BSDSRCDIR/share/mk" "$out/share/mk"
       find "$out/share/mk" -type f -print0 |
         while IFS= read -r -d "" f; do
           substituteInPlace "$f" --replace 'usr/' ""
@@ -203,14 +203,14 @@ in lib.makeScopeWithSplicing
     '';
 
     postInstall = lib.optionalString (!stdenv.targetPlatform.isFreeBSD) ''
-      boot_mk=$BSDSRCDIR/tools/build/mk
-      cp $boot_mk/Makefile.boot* $out/share/mk
-      replaced_mk=$out/share/mk.orig
-      mkdir $replaced_mk
-      mv $out/share/mk/bsd.{lib,prog}.mk $replaced_mk
+      boot_mk="$BSDSRCDIR/tools/build/mk"
+      cp "$boot_mk/Makefile.boot*" "$out/share/mk"
+      replaced_mk="$out/share/mk.orig"
+      mkdir "$replaced_mk"
+      mv "$out"/share/mk/bsd.{lib,prog}.mk "$replaced_mk"
       for m in bsd.{lib,prog}.mk; do
-        cp $boot_mk/$m $out/share/mk
-        substituteInPlace $out/share/mk/$m --replace '../../../share/mk' '../mk.orig'
+        cp "$boot_mk/$m" "$out/share/mk"
+        substituteInPlace "$out/share/mk/$m" --replace '../../../share/mk' '../mk.orig'
       done
     '';
 
