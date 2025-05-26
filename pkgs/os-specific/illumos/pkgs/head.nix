1e8c681d56c21c45c329250f7729fe47a5d6f7d0
John Ericson <John.Ericson@Obsidian.Systems>
2025-05-26 01:36:44 -0400

    FIXUP urls of patches

--- pkgs/os-specific/illumos/pkgs/head.nix
+++ pkgs/os-specific/illumos/pkgs/head.nix
@@ -23,7 +23,7 @@ mkDerivation {
   patches = [
     (fetchpatch {
       name = "linux-support.patch";
-      url = "https://github.com/illumos/illumos-gate/compare/${source.rev}...Ericson2314:illumos-gate:headers-hack.diff";
+      url = "https://github.com/illumos/illumos-gate/compare/${source.rev}...Ericson2314:illumos-gate:headers-hack^.diff";
       hash = "sha256-fCp2blv+2fgcM7ldWCJPCM4NQXAUkL7Kw7AQKJjtFrI=";
     })
   ];
